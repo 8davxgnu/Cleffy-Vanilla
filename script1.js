06efb289a8e5c91fe1ac42dd7f56716f4c0a652e
David Cheung <davxgnu@tamu.edu>
2024-06-11 01:15:55 -0500

    Set up primitive note-placing system and note buttons to music-card

---
+++ script1.js
@@ -0,0 +1,41 @@
+const baseMusicLine = document.getElementById("baseLine");
+
+// -----------------------------------------------------------
+if (baseMusicLine) {
+  console.log("Successfully found baseMusicLine");
+}
+
+
+// -----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
