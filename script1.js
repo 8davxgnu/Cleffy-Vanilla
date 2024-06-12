7e61769451cdd5afbc00006afcf0d525d24368ef
David Cheung <davxgnu@tamu.edu>
2024-06-12 02:32:39 -0500

    Successful Render of random 4 notes. Create renderNotes() and checkAnswers() functions [INCOMPLETE]

--- script1.js
+++ script1.js
@@ -1,6 +1,15 @@
+// Declare document elements needed:
 const baseMusicLine = document.getElementById('baseLine');
 
-// -----------------------------------------------------------
+const buttonA = document.getElementById("noteA");
+const buttonB = document.getElementById("noteB");
+const buttonC = document.getElementById("noteC");
+const buttonD = document.getElementById("noteD");
+const buttonE = document.getElementById("noteE");
+const buttonF = document.getElementById("noteF");
+const buttonG = document.getElementById("noteG");
+// -----------------------------------------------------------------------------------------------------------
+
 /* 
 NOTES:
 
@@ -15,9 +24,15 @@ NOTES:
 - Create array storing current iteration's correct answers
 
 - const ASCII_C = "C".charCodeAt(0);
+
+
+- Since we are positioning notes relative to middle C, translations to Notes on lines will need to be translated down by the radius of the note 
+  (b/c by starting on C, a line, the note itself is actually already its radius' away from middleC line)
+    -*** Find out if translations can be done via "center" and if this is easier.
+
 */
 // -----------------------------------------------------------------------------------------------------------
-
+// Create the key from which we will use with note values to determine notes on lines:
 let noteKey = {
   0 : "C",
   1 : "D",
@@ -29,74 +44,154 @@ let noteKey = {
 }
 
 
+const note_spacing = 4;
+
+
+
+// -----------------------------------------------------------------------------------------------------------
+
+// Output Testing:
 
 // for (let key in noteKey) {
 //   console.log(key + " " + (noteKey[key]));
 // }
 
+// console.log(buttonA.textContent);
 
-// -----------------------------------------------------------
-while (true) {
-  // Generate note nums:
-  note1_rawVal = Math.floor(Math.random() * 12);
+
+
+
+
+
+
+// -----------------------------------------------------------------------------------------------------------
+// Returns: Array with answers as Note Chars
+function renderNotes() {
   
-
-
+  // Generate note nums:
+  note1_rawVal = Math.floor(Math.random() * 13);
+  note2_rawVal = Math.floor(Math.random() * 13);
+  note3_rawVal = Math.floor(Math.random() * 13);
+  note4_rawVal = Math.floor(Math.random() * 13);
 
 
   // ---------------------------------------
   // Determine corresponding noteKey index for each note via modulo result:
   num1_noteVal = (note1_rawVal % 7);
+  num2_noteVal = (note2_rawVal % 7);
+  num3_noteVal = (note3_rawVal % 7);
+  num4_noteVal = (note4_rawVal % 7);
 
 
-  // ---------------------------------------
+  // Create Array with current iteration's correct Answers:
+  let answers = [noteKey[num1_noteVal], noteKey[num2_noteVal], noteKey[num3_noteVal],noteKey[num4_noteVal],];
+  console.log(answers);
+
+
+  // Render and translate the Notes on the music lines:
   for (let noteVal in noteKey) {
+
+
     if (noteVal == num1_noteVal) {
-      console.log(`Raw Note Value: ${note1_rawVal} | Note: ${noteKey[noteVal]}`);
-    
+      console.log(`Raw Note1 Value: ${note1_rawVal} | Note1: ${noteKey[noteVal]}`);
+
+
       // Determine # of lines and spaces to traverse:
       [num_of_spaces, num_of_lines] = [(note1_rawVal - (Math.floor(note1_rawVal / 2))), (Math.floor(note1_rawVal / 2))];
-      console.log(`spaces: ${num_of_spaces} | Lines: ${num_of_lines}`);
+      console.log(`Note1 Spaces: ${num_of_spaces} | Note1 Lines: ${num_of_lines}`);
       
+
+      // Render Note:
+      let newNote = document.createElement('span');
+      newNote.className = 'note';
+      newNote.id = 'note1';
+      
+      if (note1_rawVal % 2 == 0) {
+        newNote.style.transform = `translate(calc(${note_spacing} * 2.5rem), calc(1px - (33px/2) - (${(num_of_spaces)} * 32px) - (${num_of_lines} * 1px))`;
+        baseMusicLine.appendChild(newNote);
+      }
+      else {
+        newNote.style.transform = `translate(calc(${note_spacing} * 2.5rem), calc(1px - (33px/2) - (${(num_of_spaces)} * 32px) - (${num_of_lines} * 1px) + (16px - 1px))`;
+        baseMusicLine.appendChild(newNote);
+      }
+    }
+
+
+    if (noteVal == num2_noteVal) {
+      // Determine # of lines and spaces to traverse:
+      [num_of_spaces, num_of_lines] = [(note2_rawVal - (Math.floor(note2_rawVal / 2))), (Math.floor(note2_rawVal / 2))];
+      
+
+      // Render Note:
+      let newNote = document.createElement('span');
+      newNote.className = 'note';
+      newNote.id = 'note2';
+
+      if (note2_rawVal % 2 == 0) {
+        newNote.style.transform = `translate(calc(${note_spacing} * 5rem), calc(1px - (33px/2) - (${(num_of_spaces)} * 32px) - (${num_of_lines} * 1px))`;
+        baseMusicLine.appendChild(newNote);
+      }
+      else {
+        newNote.style.transform = `translate(calc(${note_spacing} * 5rem), calc(1px - (33px/2) - (${(num_of_spaces)} * 32px) - (${num_of_lines} * 1px) + (16px - 1px))`;
+        baseMusicLine.appendChild(newNote);
+      }
+    }
+
+
+    if (noteVal == num3_noteVal) {
+      // Determine # of lines and spaces to traverse:
+      [num_of_spaces, num_of_lines] = [(note3_rawVal - (Math.floor(note3_rawVal / 2))), (Math.floor(note3_rawVal / 2))];
+      
+
       // Render Note:
       let newNote = document.createElement('span');
 
       newNote.className = 'note';
-      
-      if (note1_rawVal % 2 == 0) {
-        newNote.style.transform = `translate(0px, calc(1px - (33px/2) - (${(num_of_spaces)} * 32px) - (${num_of_lines} * 1px))`;
+      newNote.id = 'note3';
+
+      if (note3_rawVal % 2 == 0) {
+        newNote.style.transform = `translate(calc(${note_spacing} * 7.5rem), calc(1px - (33px/2) - (${(num_of_spaces)} * 32px) - (${num_of_lines} * 1px))`;
         baseMusicLine.appendChild(newNote);
       }
       else {
-        newNote.style.transform = `translate(0px, calc(1px - (33px/2) - (${(num_of_spaces)} * 32px) - (${num_of_lines} * 1px) + (16px - 1px))`;
+        newNote.style.transform = `translate(calc(${note_spacing} * 7.5rem), calc(1px - (33px/2) - (${(num_of_spaces)} * 32px) - (${num_of_lines} * 1px) + (16px - 1px))`;
         baseMusicLine.appendChild(newNote);
       }
     }
 
     
+    if (noteVal == num4_noteVal)  {
+      // Determine # of lines and spaces to traverse:
+      [num_of_spaces, num_of_lines] = [(note4_rawVal - (Math.floor(note4_rawVal / 2))), (Math.floor(note4_rawVal / 2))];
+      
+
+      // Render Note:
+      let newNote = document.createElement('span');
+
+      newNote.className = 'note';
+      newNote.id = 'note4';
+
+      if (note4_rawVal % 2 == 0) {
+        newNote.style.transform = `translate(calc(${note_spacing} * 10rem), calc(1px - (33px/2) - (${(num_of_spaces)} * 32px) - (${num_of_lines} * 1px))`;
+        baseMusicLine.appendChild(newNote);
+      }
+      else {
+        newNote.style.transform = `translate(calc(${note_spacing} * 10rem), calc(1px - (33px/2) - (${(num_of_spaces)} * 32px) - (${num_of_lines} * 1px) + (16px - 1px))`;
+        baseMusicLine.appendChild(newNote);
+      }
+    }
+    
   }
 
 
+  return answers;
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-  
-  break;
-
-
-
-
+function checkAnswers(answerArray) {
+  buttonA.addEventListener("click", () => (console.log("Hello")))
 }
 
 
@@ -105,7 +200,16 @@ while (true) {
 
 
 
+// ---------------------------------------------------------------------------------------------------------------------------
+// Main: 
 
+function trebleStart() {
+  let answerArray = renderNotes();
+  checkAnswers(answerArray);
+}
+
+
+trebleStart();
 
 
 
